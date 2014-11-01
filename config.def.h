10d64e19eb968082a86772a35f71849b4eee6140
Alex Pilon <alp@alexpilon.ca>
2014-11-01 19:46:03 -0400

    Change default cursor colour to green

--- config.def.h
+++ config.def.h
@@ -121,7 +121,7 @@ static const char *colorname[] = {
 	/* more colors can be added after 255 to use with DefaultXX */
 	"white",
 	"black",
-	"#cccccc",
+	"green",
 	"#555555",
 	"yellow",
 	"gray50"
