b154f2bb295cec1039f8446d94014bc7b7b45de7
Alex Pilon <alp@alexpilon.ca>
2014-11-01 19:45:43 -0400

    Don't alias colours
    Just because you changed the default foreground colour to green, because
    you're a Wachowski Matrix junkie, doesn't mean that white should now be
    green. We just made the foreground green. If we ask for white, we gosh
    darn give white.
    
    Code duplication is unfortunate. One can't in C initialize an array
    member by referring to another array member.

--- config.def.h
+++ config.def.h
@@ -114,8 +114,12 @@ static const char *colorname[] = {
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
+	"white",
+	"black",
 	"#cccccc",
 	"#555555",
+	"yellow",
+	"gray50"
 };
 
 
