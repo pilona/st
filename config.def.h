d6ff6ccee87e8ee7ed52489384c207bdd49cd064
Alex Pilon <alp@alexpilon.ca>
2015-04-14 10:41:56 -0400

    Use a proper crisp monospace font
    Powerline is definitely not suckless, but it's my repo.

--- config.def.h
+++ config.def.h
@@ -6,7 +6,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Terminess Powerline:pixelsize=32:antialias=false:hint=false";
 static int borderpx = 2;
 
 /*
